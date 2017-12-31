5b2e09a52150f186c69f3525033cec3a92fc7537
Lucy <Lucy@LAPTOP-VBLU8ML4>
2017-12-31 17:36:58 +0100

    v 0.4 SpielerRouten können gelegt werden

--- Thurn und Taxis/src/model/Fassade.java
+++ Thurn und Taxis/src/model/Fassade.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 // Temporäre main klasse
 
 /**
@@ -11,38 +13,43 @@ public class Fassade {
 
     public static void main(String[]args) {
 
-        Deck deck = new Deck();
-        Ablage ablage = new Ablage();
-        OffeneKarten ok = new OffeneKarten();
+    	Map map = new Map();
+        SpielerRoute route = new SpielerRoute(map);
         
-        HandKarten hk1 = new HandKarten();
-        HandKarten hk2 = new HandKarten();
-        
-        deck.printList();
-        ablage.printList();
-        ok.printList();
-        hk1.printList();
-        hk2.printList();
-        
-        ablage.kartenVerteilen(deck);
+        Provinz p = new Provinz("joh");
+        Stadt s1 = new Stadt("Augsburg", p, 0);
+        Stadt s2 = new Stadt("Innsbruck", p, 6);
+        Stadt s3 = new Stadt("Kempten", p, 7);
+        Stadt s4 = new Stadt("Ulm", p, 19);
+        Stadt s5 = new Stadt("Sigmaringen", p, 17);
 
-        while(!ok.istVoll()) {
-        	ok.karteNachlegen(deck.karteZiehen());
+        Stadt s6 = new Stadt("heil", p, 2);
+        
+        // Liste mit Verbindungen
+        
+        route.routeLegen(s1);
+        route.routeLegen(s6);
+        route.routeLegen(s2);
+        route.routeLegen(s3);
+        route.routeLegen(s4);
+        route.routeLegen(s5);
+        route.routeLegen(s1);
+        route.routeLegen(s1);  
+        route.routeLegen(s1); 
+
+        route.routeLegen(s3);  
+        
+        route.printRoute();
+        
+        ArrayList<Stadt> list = route.routeBeenden();
+        
+        route.printRoute();
+        
+        System.out.println("---");
+        for( int i=0; i<list.size(); i++ ) {
+        	System.out.println(list.get(i).getName());
         }
         
-        hk1.karteZiehen(deck.karteZiehen());
-        hk1.karteZiehen(deck.karteZiehen());
-        hk1.karteZiehen(deck.karteZiehen());
-        
-        hk2.karteZiehen(ok.karteZiehen(3, deck.karteZiehen()));
-        hk2.karteZiehen(ok.karteZiehen(3, deck.karteZiehen()));
-        hk2.karteZiehen(deck.karteZiehen());
-        
-        deck.printList();
-        ablage.printList();
-        ok.printList();
-        hk1.printList();
-        hk2.printList();
-        ok.printList();
+
     }
 }
--- Thurn und Taxis/src/model/SpielerRoute.java
+++ Thurn und Taxis/src/model/SpielerRoute.java
@@ -1,24 +1,137 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Bruacht zum initialisieren die Map um zu pruefen ob Routen realisierbar sind.
  * Speichert und verwaltet die aktuelle Route des Spielers.
+ * Arbeitet intern mit den Staedte-ID´S.
  * 
  * @author Walter
  *
  */
 public class SpielerRoute {
 
-	private int anfang;
-	private int ende;
-	private List<Stadt> staedte;
+	private Map map;
 	
-	public void routeLegen() {
+	private Stadt anfang;
+	private Stadt ende;
+	private ArrayList<Stadt> staedte;
+	
+	SpielerRoute( Map map ) {
+		this.map = map;
+		staedte = new ArrayList<Stadt>();
+	}
+	
+	/**
+	 * Zuerst wird geprueft ob eine neue Route gelegt werden muss 
+	 * oder eine alte weitergefuehrt wird. Wenn sie weitergefuehrt wird
+	 * prueft Map ob das moeglich ist. Dabei wird geprueft ob die HandKarte mit
+	 * dem Start oder End Knoten der Route verbunden werden kann.
+	 * 
+	 * @param stadt wird von den Handkarten uebergeben
+	 * @return Stadt gibt die Stadt zurueck an die verbunden wurde
+	 */
+	public Stadt routeLegen( Stadt stadt ) {
 		
+		if(routeIstLeer()) {
+			return routeNeuStarten(stadt);
+		}
+		else {
+			return routeWeiterFuehren(stadt);
+		}
+	}
+	
+	private Stadt routeNeuStarten( Stadt stadt ) {
+		
+		anfang = stadt;
+		ende = stadt;
+		
+		staedte.add(stadt);
+		
+		return stadt;
+	}
+	
+	
+	
+	
+	// ######## PRÜFE OB ROUTE SCHON IN LISTE DRIN ####
+	
+	
+	
+	/**
+	 * Prueft ob Route moeglich ist und gibt neue Verbindung zurueck.
+	 * @param stadt von den Handkarten
+	 * @return Stadt mit der verbunden wurde
+	 */
+	private Stadt routeWeiterFuehren( Stadt stadt ) {
+		
+		int id = stadt.getID();
+		
+		if( staedte.contains(stadt)) {
+			System.out.println("SpielerRoute: Stadt ist bereits in der Route.");
+			return null;
+		}
+		
+		if( map.istVerbunden( anfang.getID(), id ) ) {
+			staedte.add(stadt);
+			
+			Stadt verbindung = anfang;
+			anfang = stadt;
+			
+			return verbindung;
+		}
+		
+		if( map.istVerbunden( ende.getID(), id ) ) {
+			staedte.add(stadt);
+
+			Stadt verbindung = ende;
+			ende = stadt;
+			return verbindung;
+		}
+		
+		System.out.println("SpielerRoute: Diese Route kann nicht gelegt werden");
+		return null;
 	}
 
 	public void routeVerwerfen() {
+		anfang = null;
+		ende = null;
+		while(!staedte.isEmpty()) {
+			staedte.remove(staedte.size()-1);
+		}
+	}
+	
+	
+	
+	
+	// ### List kann noch nicht zurück gegeben werden ######################## //
+	
+	/**
+	 * Gibt eine Liste der in der Route enthaltenen Staedte zurueck.
+	 */
+	public ArrayList<Stadt> routeBeenden() {
 		
+		ArrayList<Stadt> list = new ArrayList<Stadt>();
+
+		for( int i=0; i<list.size()-1; i++ ) {
+			list.set(i, staedte.get(i));
+		}
+		
+		routeVerwerfen();
+		
+		return list;		
+	}
+	
+	public boolean routeIstLeer() {
+		return staedte.size() == 0;
+	}
+	
+	public void printRoute() {
+		System.out.println("###SpielerRoute###");
+		for( int i=0; i<staedte.size(); i++ ) {
+			System.out.println(staedte.get(i).getName());
+		}
 	}
 }
